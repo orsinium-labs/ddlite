8d8b6c23d6392926d3016fc5301086e0457c8d74
gram <git@orsinium.dev>
2023-07-09 09:35:46 +0200

    drop dead code

--- expr.go
+++ expr.go
@@ -8,16 +8,20 @@ import (
 
 type Models []any
 
+// Expr is an SQL expression. I can be used as part of SQL queries.
 type Expr[T any] interface {
 	Default() T
 	Squirrel(Models) squirrel.Sqlizer
 }
 
+// tFunc is a private type to represent stored function expression.
+// `T` is the type of the function return value.
 type tFunc[T any] struct {
 	Name string
 	Args []any
 }
 
+// F is a stored function.
 func F[T any](name string, args ...any) Expr[T] {
 	return tFunc[T]{Name: name, Args: args}
 }
@@ -30,10 +34,12 @@ func (fn tFunc[T]) Squirrel(ms Models) squirrel.Sqlizer {
 	return squirrel.Expr(fmt.Sprintf("%s(?)", fn.Name), fn.Args...)
 }
 
+// tCol is aprivate type to represent a column name expression.
 type tCol[T any] struct {
 	val *T
 }
 
+// C is a column.
 func C[T any](val *T) Expr[T] {
 	return tCol[T]{val: val}
 }
@@ -52,10 +58,12 @@ func (col tCol[T]) Squirrel(ms Models) squirrel.Sqlizer {
 	panic("uknown column")
 }
 
+// tVal is a private type to represent a literal value expression.
 type tVal[T any] struct {
 	val T
 }
 
+// V is a literal value.
 func V[T any](val T) Expr[T] {
 	return tVal[T]{val: val}
 }
--- stringify.go
+++ stringify.go
@@ -2,37 +2,9 @@ package sequel
 
 import (
 	"errors"
-	"fmt"
 	"reflect"
 	"strings"
-
-	"github.com/Masterminds/squirrel"
 )
-
-// asSquirrel converts the given value to a squirrel expression.
-//
-// The given value can be a DB function, model field, or a literal value.
-func asSquirrel(m any, f any) squirrel.Sqlizer {
-	// function
-	switch fn := f.(type) {
-	case tFunc[int]:
-		args := make([]any, 0, len(fn.Args))
-		for _, arg := range fn.Args {
-			args = append(args, asSquirrel(m, arg))
-		}
-		return squirrel.Expr(fmt.Sprintf("%s(?)", fn.Name), args...)
-	default:
-	}
-
-	// model field
-	fname, err := getFieldName(m, f)
-	if err == nil {
-		return squirrel.Expr(fname)
-	}
-
-	// literal value
-	return squirrel.Expr("?", f)
-}
 
 func getModelName(model any) string {
 	t := reflect.ValueOf(model).Elem().Type()
@@ -53,11 +25,4 @@ func getFieldName(model any, field any) (string, error) {
 		}
 	}
 	return "", errors.New("field not found")
-}
-
-// Ref converts the given value to a pointer.
-//
-// Convenient for making a pointer to a literal value.
-func Ref[T any](val T) *T {
-	return &val
 }
