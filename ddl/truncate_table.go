7043970b8fefc3319226e9a253a17085759ad7f4
gram <git@orsinium.dev>
2023-10-22 12:21:37 +0200

    add TruncateTable

---
+++ ddl/truncate_table.go
@@ -0,0 +1,32 @@
+package ddl
+
+import (
+	"fmt"
+
+	"github.com/Masterminds/squirrel"
+	"github.com/orsinium-labs/sequel/dbconf"
+	"github.com/orsinium-labs/sequel/internal"
+)
+
+type tTruncateTable struct {
+	model internal.Model
+}
+
+// TruncateTable builds TRUNCATE TABLE query that removes all data from the table.
+func TruncateTable[T internal.Model](model *T) tTruncateTable {
+	return tTruncateTable{
+		model: model,
+	}
+}
+
+func (q tTruncateTable) Squirrel(conf dbconf.Config) (squirrel.Sqlizer, error) {
+	// https://en.wikipedia.org/wiki/Data_definition_language#TRUNCATE_statement
+	// https://www.sqlite.org/lang_delete.html
+	tableName := internal.GetTableName(conf, q.model)
+	prefix := "TRUNCATE TABLE"
+	if conf.Dialect == dbconf.SQLite {
+		prefix = "DELETE FROM"
+	}
+	sql := fmt.Sprintf("%s %s", prefix, tableName)
+	return squirrel.Expr(sql), nil
+}
