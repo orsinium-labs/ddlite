0e0a1db7bb01d6bb0b22a18273a516d7424c603e
gram <git@orsinium.dev>
2023-10-26 13:09:03 +0200

    make switch builder public

--- dml/expr_switch.go
+++ dml/expr_switch.go
@@ -5,37 +5,37 @@ import (
 	"github.com/orsinium-labs/sequel/internal/tokens"
 )
 
-type tSwitch[T, R any] struct {
+type SwitchBuilder[T, R any] struct {
 	val     Expr[T]
 	conds   []Expr[T]
 	results []Expr[R]
 	def     Expr[R]
 }
 
-func Switch[R any]() tSwitch[bool, R] {
-	return tSwitch[bool, R]{}
+func Switch[R any]() SwitchBuilder[bool, R] {
+	return SwitchBuilder[bool, R]{}
 }
 
-func SwitchBy[R, T any](val Expr[T]) tSwitch[T, R] {
-	return tSwitch[T, R]{val: val}
+func SwitchBy[R, T any](val Expr[T]) SwitchBuilder[T, R] {
+	return SwitchBuilder[T, R]{val: val}
 }
 
-func (s tSwitch[T, R]) Case(cond Expr[T], res Expr[R]) tSwitch[T, R] {
+func (s SwitchBuilder[T, R]) Case(cond Expr[T], res Expr[R]) SwitchBuilder[T, R] {
 	s.conds = append(s.conds, cond)
 	s.results = append(s.results, res)
 	return s
 }
 
-func (s tSwitch[T, R]) Else(res Expr[R]) tSwitch[T, R] {
+func (s SwitchBuilder[T, R]) Else(res Expr[R]) SwitchBuilder[T, R] {
 	s.def = res
 	return s
 }
 
-func (tSwitch[T, R]) ExprType() R {
+func (SwitchBuilder[T, R]) ExprType() R {
 	return *new(R)
 }
 
-func (s tSwitch[T, R]) Tokens(c dbconf.Config) tokens.Tokens {
+func (s SwitchBuilder[T, R]) Tokens(c dbconf.Config) tokens.Tokens {
 	ts := tokens.New(tokens.Keyword("CASE"))
 	if s.val != nil {
 		ts.Extend(s.val.Tokens(c))
