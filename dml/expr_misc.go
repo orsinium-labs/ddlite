1ecee4cbf0fcf6756197068f179d23a3f6bb1ec5
gram <git@orsinium.dev>
2023-10-26 08:38:28 +0200

    add Not

--- dml/expr_misc.go
+++ dml/expr_misc.go
@@ -21,10 +21,30 @@ func (expr tSuffix[T, R]) Tokens(c dbconf.Config) tokens.Tokens {
 	return ts
 }
 
+type tPrefix[T, R any] struct {
+	prefix string
+	right  Expr[T]
+}
+
+func (tPrefix[T, R]) ExprType() R {
+	return *new(R)
+}
+
+func (expr tPrefix[T, R]) Tokens(c dbconf.Config) tokens.Tokens {
+	ts := tokens.New()
+	ts.Add(tokens.Keyword(expr.prefix))
+	ts.Extend(expr.right.Tokens(c))
+	return ts
+}
+
 func IsNull[T any](val Expr[T]) Expr[bool] {
-	return tSuffix[T, bool]{val, "ISNULL"}
+	return tSuffix[T, bool]{val, "IS NULL"}
 }
 
 func IsNotNull[T any](val Expr[T]) Expr[bool] {
-	return tSuffix[T, bool]{val, "ISNOTNULL"}
+	return tSuffix[T, bool]{val, "IS NOT NULL"}
+}
+
+func Not(val Expr[bool]) Expr[bool] {
+	return tPrefix[bool, bool]{"NOT", val}
 }
