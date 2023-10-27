02d2826d30feb42b4eb0d2d0bd081ccf10a66ac8
gram <git@orsinium.dev>
2023-10-27 20:18:26 +0200

    add parens to where if needed

--- dml/clauses.go
+++ dml/clauses.go
@@ -15,11 +15,20 @@ func (c whereClause) buildWhere(conf dbconf.Config) tokens.Tokens {
 		return ts
 	}
 	ts.Add(tokens.Keyword("WHERE"))
+	precAnd := conf.Dialect.Precedence("AND")
 	for i, pred := range c.predicates {
 		if i > 0 {
 			ts.Add(tokens.Keyword("AND"))
 		}
+		precPred := pred.Precedence(conf)
+		addParens := precAnd == 0 || precPred < precAnd
+		if addParens {
+			ts.Add(tokens.LParen())
+		}
 		ts.Extend(pred.Tokens(conf))
+		if addParens {
+			ts.Add(tokens.RParen())
+		}
 	}
 	return ts
 }
