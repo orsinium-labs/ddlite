699ad2b0b605437422d8c42dd0100bbc673f4888
gram <git@orsinium.dev>
2023-11-07 17:38:57 +0100

    report unsupported column type

--- ddl/column_def.go
+++ ddl/column_def.go
@@ -2,6 +2,7 @@ package ddl
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/orsinium-labs/sequel-ddl/dialects"
@@ -57,6 +58,11 @@ func (def tColumn) Collate(collationName string) tColumn {
 func (def tColumn) tokens(dialect dialects.Dialect) tokens.Tokens {
 	constraints := strings.Join(def.constraints, " ")
 	colSQL := def.colType(dialect)
+	if colSQL == "" {
+		const msg = "the data type used for the column '%s' is not supported by the dialect"
+		err := fmt.Errorf(msg, def.name)
+		return tokens.New(tokens.Err(err))
+	}
 	ts := tokens.New(
 		tokens.ColumnName(def.name),
 		tokens.Raw(colSQL),
