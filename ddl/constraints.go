f0e336a53ebe58a47b58750122357edb97a58dcc
gram <git@orsinium.dev>
2023-11-10 09:54:23 +0100

    add PrimaryKey constrain

--- ddl/constraints.go
+++ ddl/constraints.go
@@ -1,8 +1,6 @@
 package ddl
 
 import (
-	"errors"
-
 	"github.com/orsinium-labs/sequel-ddl/dialects"
 	"github.com/orsinium-labs/sequel-ddl/internal/tokens"
 )
@@ -16,22 +14,40 @@ type tUnique struct {
 	names []Safe
 }
 
-func Unique(names ...Safe) Constraint {
-	return tUnique{names: names}
+func Unique(name Safe, names ...Safe) Constraint {
+	return tUnique{names: append([]Safe{name}, names...)}
 }
 
 func (def tUnique) isConstraint() {}
 
 func (def tUnique) tokens(dialects.Dialect) tokens.Tokens {
-	if len(def.names) == 0 {
-		err := errors.New("unique index must have at least one column specified")
-		return tokens.New(tokens.Err(err))
-	}
-	ts := tokens.New(
+	return tokens.New(
 		tokens.Keyword("UNIQUE"),
 		tokens.LParen(),
 		tokens.Raws(def.names...),
 		tokens.RParen(),
 	)
-	return ts
+}
+
+type tPrimaryKey struct {
+	names []Safe
+}
+
+// Mark multiple columns as a compound primary key.
+//
+// If you want the table to have a single-column primary key,
+// use [ColumnBuilder.PrimaryKey] instead.
+func PrimaryKey(name1, name2 Safe, names ...Safe) Constraint {
+	return tPrimaryKey{names: append([]Safe{name1, name2}, names...)}
+}
+
+func (def tPrimaryKey) isConstraint() {}
+
+func (def tPrimaryKey) tokens(dialects.Dialect) tokens.Tokens {
+	return tokens.New(
+		tokens.Keyword("PRIMARY KEY"),
+		tokens.LParen(),
+		tokens.Raws(def.names...),
+		tokens.RParen(),
+	)
 }
